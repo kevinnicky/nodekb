00cbbd2a8e98eb7c0a8ccb97d999b53017df9454
Kevin Nicky Setiawan <chrysaintcthenum@gmail.com>
2020-04-12 10:24:24 +0700

    Delete require commit from passport.js

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,5 @@
 const localStrategy = require('passport-local').Strategy;
 const User = require('../models/user');
-const config = require('../config/database');
 const bcrypt = require('bcryptjs');
 
 module.exports = (passport) => {
